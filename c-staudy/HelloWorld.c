c8e8ee230b3082f0fe581ce337231628c60bf762
TKS-NKMR <45942464+TKS-NKMR@users.noreply.github.com>
2019-01-12 22:32:45 +0900

    Update HelloWorld.c

--- c-staudy/HelloWorld.c
+++ c-staudy/HelloWorld.c
@@ -4,6 +4,12 @@ int print(char*);
 
 int main(void)
 {
-	print("Hellow ikezaki\n");
+	printf("What's your name?\n");
+
+	char name[50];
+	scanf("%s", name);
+
+	printf("Hello %s, welcome to c language!\n", name);
+
 	return 0;
 }
