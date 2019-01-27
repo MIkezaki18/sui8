8f7ccd4c5501ffbf4cdcefc364b2c2de6e3475c5
root <root@localhost.localdomain>
2019-01-27 15:35:36 +0900

    practice01 add

---
+++ c-staudy/c01-QuickSort/ike/quickSort.c
@@ -0,0 +1,18 @@
+int Select(int *x, int n, int k);
+
+int main()
+{
+	int	x[10];
+	int	n=0,k=0;
+
+	Select(x,n,k);
+	
+	return 0;
+}
+
+int Select(int *x, int n, int k)
+{
+	int ret = 0;
+
+	return ret;
+}
